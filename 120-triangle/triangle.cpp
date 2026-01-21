d56effd186305a82114cbe00bc214dbb3740d837
Navneet verma <vermamanu52528591@gmail.com>
2026-01-21 21:15:19 +0530

    Time: 4 ms (7.09%) | Memory: 13.6 MB (8.19%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -3,21 +3,22 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
         int m = triangle[n-1].size();
-        vector<vector<int>>dp(n,vector<int>(m,0));
-        dp[0][0] = triangle[0][0];
+      
+        vector<int>prevRow(1,0);
+        prevRow[0] = triangle[0][0];
         for(int i = 1;i<n;i++){
-            dp[i][0] = dp[i-1][0] + triangle[i][0];
-        }
-        for(int i = 1;i<n;i++){
+            vector<int>currRow(triangle[i].size(),0);
+            currRow[0] =triangle[i][0] + prevRow[0];
             for(int j =1;j<=i;j++){
                 int top = INT_MAX;
-                int diag = dp[i-1][j-1];
-                if(i!=j) top = dp[i-1][j];                
-                dp[i][j] = triangle[i][j] + min(top, diag);
+                int diag = prevRow[j-1];
+                if(i!=j) top = prevRow[j];                
+                currRow[j] = triangle[i][j] + min(top, diag);
             }
+            prevRow = currRow;
         }
         int ans = INT_MAX;
-        for(auto it: dp[n-1]){
+        for(auto it:prevRow){
             ans = min(ans,it);
         }
         return ans;
