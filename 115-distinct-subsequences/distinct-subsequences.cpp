f59b95c29fc16857a45cde9d675e59bc544e106b
Navneet verma <vermamanu52528591@gmail.com>
2026-01-28 19:19:56 +0530

    Time: 39 ms (19.90%) | Memory: 44.1 MB (39.76%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,23 +1,42 @@
 class Solution {
 public:
-vector<vector<int>>dp;
-int solve(string &s,string &t, int n1, int n2){
-    if(n2==0)return 1;
-    if(n1==0)return 0;
-    if(dp[n1][n2]!=-1)return dp[n1][n2];
-    if(s[n1-1]==t[n2-1]){
-        return  dp[n1][n2] = solve(s, t, n1-1, n2-1) + solve(s, t, n1-1, n2);
-    }
-    else{
-        return  dp[n1][n2] = solve(s, t, n1-1, n2);
-    }
-}
+using ll = long long;
+vector<vector<ll>>dp;
+// int solve(string &s,string &t, int n1, int n2){
+//     if(n2==0)return 1;
+//     if(n1==0)return 0;
+//     if(dp[n1][n2]!=-1)return dp[n1][n2];
+//     if(s[n1-1]==t[n2-1]){
+//         return  dp[n1][n2] = solve(s, t, n1-1, n2-1) + solve(s, t, n1-1, n2);
+//     }
+//     else{
+//         return  dp[n1][n2] = solve(s, t, n1-1, n2);
+//     }
+// }
 
 int numDistinct(string s, string t) {
     int n1 = s.size();
     int n2 = t.size();
     if(n2 > n1)return 0;
-    dp.assign(n1+1,vector<int>(n2+1,-1));
-    return solve(s, t, n1, n2);
+    dp.assign(n2+1,vector<ll>(n1+1,0));
+
+    for(int i = 0;i<n1+1;i++){
+        dp[0][i] = 1;
+    }
+
+    for(int i = 1;i<n2+1;i++){
+        for(int j = 1;j<n1+1;j++){
+            if(t[i-1]== s[j-1]){
+                dp[i][j] = dp[i-1][j-1] + dp[i][j-1];
+            }
+            else{
+                dp[i][j] = dp[i][j-1];
+            }
+             if (dp[i][j] > INT_MAX) {
+                    dp[i][j] = INT_MAX;
+                }
+        }
+    }
+    return (int)dp[n2][n1];
     }
 };
