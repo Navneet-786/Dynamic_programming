f09c28aff6ae47293de95cdb3bc0f6f8dd6a408f
Navneet verma <vermamanu52528591@gmail.com>
2026-01-28 19:24:22 +0530

    Time: 17 ms (78.10%) | Memory: 9.5 MB (78.84%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
 using ll = long long;
-vector<vector<ll>>dp;
 int numDistinct(string s, string t) {
     int n1 = s.size();
     int n2 = t.size();
     if(n2 > n1)return 0;
-    dp.assign(n2+1,vector<ll>(n1+1,0));
-
-    for(int i = 0;i<n1+1;i++){
-        dp[0][i] = 1;
-    }
-
+    vector<ll>prevRow(n1+1,1);
+    vector<ll>currRow(n1+1);
+    currRow[0] = 0;
     for(int i = 1;i<n2+1;i++){
         for(int j = 1;j<n1+1;j++){
             if(t[i-1]== s[j-1]){
-                dp[i][j] = dp[i-1][j-1] + dp[i][j-1];
+                currRow[j] = prevRow[j-1] + currRow[j-1];
             }
             else{
-                dp[i][j] = dp[i][j-1];
+                currRow[j] = currRow[j-1];
             }
-            if (dp[i][j] > INT_MAX) {
-                dp[i][j] = INT_MAX;
+            if (currRow[j] > INT_MAX) {
+                currRow[j] = INT_MAX;
             }
         }
+        prevRow = currRow;
     }
-    return (int)dp[n2][n1];
+    return prevRow[n1];
     }
 };
